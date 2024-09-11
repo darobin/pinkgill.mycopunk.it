85bde68e304f7ef052d9089a08dbf3c90290c289
Robin Berjon <robin.berjon@nytimes.com>
2024-09-11 14:10:01 +0200

    login page

--- client/el/root-routes.js
+++ client/el/root-routes.js
@@ -9,7 +9,7 @@ export class PinkgillRoot extends withStores(LitElement, [$computedRoute]) {
       :host {
         display: block;
       }
-      .loading {
+      .loading, .login {
         position: absolute;
         top: 0;
         bottom: 0;
@@ -19,6 +19,22 @@ export class PinkgillRoot extends withStores(LitElement, [$computedRoute]) {
         align-items: center;
         justify-content: center;
       }
+      .login h2 {
+        font-family: var(--header-fam);
+        font-size: 1rem;
+        font-weight: 900;
+        text-decoration: underline;
+        text-decoration-color: var(--electric-bright);
+        text-decoration-thickness: 2px;
+        margin: 0;
+      }
+      .login form {
+        display: flex;
+        gap: var(--sl-spacing-small);
+      }
+      .login sl-input {
+        min-width: 400px;
+      }
     `
   ];
   render () {
@@ -27,7 +43,15 @@ export class PinkgillRoot extends withStores(LitElement, [$computedRoute]) {
       case 'loading':
         return html`<div class="loading"><pg-loading></pg-loading></div>`;
       case 'login':
-          return html`login`;
+          return html`<div class="login">
+            <sl-card>
+              <h2 slot="header">login</h2>
+              <form action="/api/login">
+                <sl-input name="handle" placeholder="Enter your handle (e.g. alice.bsky.social)" required></sl-input>
+                <sl-button type="submit" variant="primary">Log in</sl-button>
+              </form>
+            </sl-card>
+          </div>`;
       case 'home':
         return html`home`;
       case '404':
--- public/pinkgill.js
+++ public/pinkgill.js
@@ -1,6 +1,8 @@
 
 import { loadIdentity, $isLoggedIn, $loginLoading } from '../client/store/identity.js';
 
+import '@shoelace-style/shoelace';
+
 import '../client/el/root-routes.js';
 import '../client/el/404.js';
 import '../client/el/loading.js';
