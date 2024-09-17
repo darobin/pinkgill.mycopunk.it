a1465e35c802e4366cded32989efd83ae04ac514
Robin Berjon <robin.berjon@nytimes.com>
2024-09-17 17:44:52 +0200

    v1

--- client/el/tile.js
+++ client/el/tile.js
@@ -10,6 +10,37 @@ export class PinkgillTile extends LitElement {
       :host {
         display: block;
       }
+      div[slot="header"] {
+        display: grid;
+        grid-template-areas:
+          "a b"
+          "c b"
+        ;
+      }
+      h3 {
+        grid-area: a;
+        font-family: var(--header-fam);
+        font-size: 1rem;
+        margin: 0;
+      }
+      .handle {
+        grid-area: b;
+        font-weight: bold;
+        line-height: 1;
+      }
+      time {
+        grid-area: c;
+        font-size: 0.8rem;
+        color: var(--sl-color-neutral-500);
+      }
+      sl-card::part(body) {
+        padding: 0;
+      }
+      iframe {
+        width: 100%;
+        height: 500px;
+        border: 0;
+      }
     `,
   ];
   render () {
@@ -20,7 +51,7 @@ export class PinkgillTile extends LitElement {
     return html`<sl-card>
       <div slot="header">
         <h3>${this.tile.name}</h3>
-        <strong>@${this.tile.handle}</strong>
+        <span class="handle">@${this.tile.handle}</span>
         <time datetime=${this.tile.createdAt}>${this.tile.createdAt}</time>
       </div>
       <iframe src=${url}></iframe>
