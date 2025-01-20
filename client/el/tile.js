5cda553bc38b3533e79158c3216f6c306e511a24
Robin Berjon <robin.berjon@nytimes.com>
2025-01-20 17:58:41 +0100

    delete menu but unwired

--- client/el/tile.js
+++ client/el/tile.js
@@ -1,13 +1,15 @@
 
 import { LitElement, html, css, nothing } from 'lit';
 import { until } from 'lit/directives/until.js';
-// import { MultiStoreController } from '@nanostores/lit';
+import { StoreController } from '@nanostores/lit';
 import { format } from 'timeago.js';
 import { urlForTile } from '../store/tiles.js';
 // import { isInstallable, makeInstaller, $installs } from '../store/installs.js';
+import { $identity } from '../store/identity.js';
 import { buttons, errors } from './styles.js';
 
 export class PinkgillTile extends LitElement {
+  #identity = new StoreController(this, $identity);
   static properties = {
     tile: { attribute: false },
     wish: { attribute: false },
@@ -40,7 +42,29 @@ export class PinkgillTile extends LitElement {
         flex-grow: 1;
         margin-left: 12px;
       }
+      .header {
+        display: grid;
+        grid-template-areas:
+          "a b"
+          "c b"
+        ;
+      }
+      h3 {
+        grid-area: a;
+        font-family: var(--header-fam);
+        margin: 0;
+        padding-top: 0.5rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      .menu {
+        grid-area: b;
+        text-align: right;
+        align-content: center;
+      }
       .meta {
+        grid-area: c;
         padding: 0 0 4px 0;
         letter-spacing: 0;
         font-size: 15px;
@@ -72,16 +96,11 @@ export class PinkgillTile extends LitElement {
       section:last-of-type .avatar {
         background: none;
       }
-      h3 {
-        font-family: var(--header-fam);
-        margin: 0;
-        padding-top: 0.5rem;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
       sl-icon-button {
         font-size: 1.4rem;
+      }
+      sl-dropdown > sl-button::part(base) {
+        border: none;
       }
       iframe {
         width: 100%;
@@ -143,23 +162,45 @@ export class PinkgillTile extends LitElement {
     alert('Installation has been disconnected');
     // await this.#installerData.installTile(this.tile);
   }
+  async handleContextual (evt) {
+    const item = evt?.detail?.item?.value;
+    if (item === 'delete') {
+      // XXX
+      // note that we don't delete anything, we just mark as deleted
+      // otherwise, you could uninstall an original tile and kill all its instances
+      alert('Delete!');
+    }
+  }
   renderContainer (content, footer) {
     console.warn(this.tile, this.wish);
     // XXX Not sure why this branch
     if (this.wish?.can === 'instantiate') return html`<div>${content}</div>`
     const manifest = this.tile?.manifest;
     const profile = this.tile?.profile;
+    const canDelete = (this.#identity.value.did === profile?.did);
+    let menu = nothing;
+    if (canDelete) {
+      menu = html`<sl-dropdown placement="bottom-end">
+        <sl-button slot="trigger" caret></sl-button>
+        <sl-menu @sl-select=${this.handleContextual}>
+          <sl-menu-item value="delete">Delete Tile</sl-menu-item>
+        </sl-menu>
+      </sl-dropdown>`;
+    }
     return html`<section>
     <div class="avatar">
       <a href=${`/user/${profile?.handle}`}><img src=${profile?.avatar} alt=${profile?.displayName || profile?.handle}></a>
     </div>
     <div class="post">
-      <h3>${manifest.name}</h3>
-      <div class="meta">
-        <a href=${`/user/${profile?.handle}`} class="name">${profile?.displayName || profile?.handle}</a>
-        <a href=${`/user/${profile?.handle}`} class="handle">@${profile?.handle}</a>
-        ·
-        <time datetime=${this.tile?.createdAt}>${format(this.tile?.createdAt)}</time></a>
+      <div class="header">
+        <h3>${manifest.name}</h3>
+        <div class="meta">
+          <a href=${`/user/${profile?.handle}`} class="name">${profile?.displayName || profile?.handle}</a>
+          <a href=${`/user/${profile?.handle}`} class="handle">@${profile?.handle}</a>
+          ·
+          <time datetime=${this.tile?.createdAt}>${format(this.tile?.createdAt)}</time></a>
+        </div>
+        <div class="menu">${menu}</div>
       </div>
       <div class="content">${content || nothing}</div>
       ${footer ? html`<div class="footer">${footer}</div>` : nothing}
